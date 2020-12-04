92531ee0aa64551ce0b1cdac20da3ec4b0748825
marco <g.marcotaba@gmail.com>
2020-12-04 22:00:32 +0100

    added array rest + next button
    need to fix bugs

--- script.js
+++ script.js
@@ -5,25 +5,27 @@ const myApp = new Vue ({
       searched: '',
       stars: 5,
       apikey: 'b1c34fb435544b294b8ba79826dd86ac',
-      uriImg: 'https://image.tmdb.org/t/p/w500/'
+      uriImg: 'https://image.tmdb.org/t/p/w500/',
+      nextPg: 1
     },
     methods: {
         movieSearch() {
-
+          this.allMovies = []; //reset dell'array
+          
       //Get for movie
-      axios.get("https://api.themoviedb.org/3/search/movie",
-      {params: {api_key: this.apikey,
+      axios.get("https://api.themoviedb.org/3/search/movie", {
+        params: {'api_key': this.apikey,
                 query: this.searched,
-                language: 'it'
+                page: this.nextPage,
       }})
-      .then(response => {this.allMovies = this.allMovies.concat(response.data.results);
+      .then((response) => {this.allMovies = response.data.results;
       });
 
       //Get for Tv Series
       axios.get("https://api.themoviedb.org/3/search/tv",
-      {params: {api_key: this.apikey,
+      {params: {'api_key': this.apikey,
                 query: this.searched,
-                language: 'it'
+                page: this.nextPage,
       }})
       .then(response => {this.allMovies = this.allMovies.concat(response.data.results);
       });
@@ -33,6 +35,20 @@ const myApp = new Vue ({
       },
       notFound(el) {
         el.target.src = "img/notfound.png";
+      },
+      nextPage() {
+        this.nextPg++;
+        this.movieSearch();
+        return this.nextPg;
       }
-    }
-})
+    },
+    mounted() {
+      axios.get("https://api.themoviedb.org/3/movie/popular", 
+      {params:{'api_key': this.apikey,
+                page: this.nextPage,
+              }})
+      .then(resp => {this.allMovies = resp.data.results;
+      })
+      }
+  })
+
