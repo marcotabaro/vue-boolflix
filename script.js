cd7385ea1f441c5d8bf87c0093f91985cbd6316e
marco <g.marcotaba@gmail.com>
2020-12-09 00:55:25 +0100

    modified logic and some style
    need to work more about the style

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
 const myApp = new Vue ({
     el:"#root",
     data: {
-      allMovies: [],
+      multiSearch: [],
       searched: '',
       stars: 5,
       apikey: 'b1c34fb435544b294b8ba79826dd86ac',
-      uriImg: 'https://image.tmdb.org/t/p/w500/',
-      nextPg: 1
+      uriImg: 'https://image.tmdb.org/t/p/w342/',
+      currPage: 1,
+      totPages: 1
     },
     methods: {
-        movieSearch() {
-          this.allMovies = []; //reset dell'array
-          
-      //Get for movie
-      axios.get("https://api.themoviedb.org/3/search/movie", {
+        search() {
+
+         axios.get("https://api.themoviedb.org/3/search/multi", {
         params: {'api_key': this.apikey,
                 query: this.searched,
-                page: this.nextPage,
+                page: this.currPage,
       }})
-      .then((response) => {this.allMovies = response.data.results;
+      .then((response) => {this.multiSearch = response.data.results;
+                           this.totPages = response.data.total_pages;
       });
 
-      //Get for Tv Series
-      axios.get("https://api.themoviedb.org/3/search/tv",
-      {params: {'api_key': this.apikey,
-                query: this.searched,
-                page: this.nextPage,
-      }})
-      .then(response => {this.allMovies = this.allMovies.concat(response.data.results);
-      });
-      },
-      starRating(rate) {
-        return Math.ceil(rate / 2);
-      },
-      notFound(el) {
-        el.target.src = "img/notfound.png";
-      },
-      nextPage() {
-        this.nextPg++;
-        this.movieSearch();
-        return this.nextPg;
-      }
     },
-    mounted() {
-      axios.get("https://api.themoviedb.org/3/movie/popular", 
-      {params:{'api_key': this.apikey,
-                page: this.nextPage,
-              }})
-      .then(resp => {this.allMovies = resp.data.results;
-      })
+      starRating(rate) {
+        return Math.ceil(rate / 2)
+    },
+      notFound(el) {
+        el.target.src = "img/notfound.png"
+    },
+    scrollSearch() {
+      window.onscroll = () => {
+        let bottomWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+
+        if(this.currPage <= this.totPages){
+        if (bottomWindow) {
+          this.currPage++;
+            axios.get("https://api.themoviedb.org/3/search/multi", {
+            params: {'api_key': this.apikey,
+                query: this.searched,
+                page: this.currPage,
+      },
+    })
+      .then((response) => {
+        let array = response.data.results;
+        this.multiSearch = [...this.multiSearch, ...array];
+          });
+        }
       }
-  })
+    }
+  }
+},
+    mounted() {
+      this.scrollSearch();
+        axios.get("https://api.themoviedb.org/3/movie/popular", 
+        {params:{'api_key': this.apikey,
+                  page: this.currPage,
+                }})
+        .then(response => {this.multiSearch = response.data.results;
+          this.totPages = response.data.total_pages;
+        })
+  }
+})
 
